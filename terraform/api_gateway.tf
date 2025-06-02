0bff53907bdb7d0c3f58a4de0ae48b6567442668
Alex-Trauthman <alextrauthman.full@gmail.com>
2025-06-02 18:13:30 -0300

    initial commit

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -31,6 +31,7 @@ resource "aws_api_gateway_integration_response" "lambda_integration_response" {
   response_templates = {
     "application/json" = "$input.json('$')"
   }
+  depends_on = [aws_api_gateway_integration.lambda_integration]
 }
 resource "aws_api_gateway_integration" "lambda_integration" {
   rest_api_id             = aws_api_gateway_rest_api.this.id
